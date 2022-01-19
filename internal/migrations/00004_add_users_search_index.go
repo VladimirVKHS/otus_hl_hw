26f82453134a786559ab823c997979aa9026209d
Vladimir <kovalev.home@mail.ru>
2022-01-20 00:10:22 +0300

    [+] Users search index

---
+++ internal/migrations/00004_add_users_search_index.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"database/sql"
+	goose "github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigration(upAddUsersSearchIndex, downAddUsersSearchIndex)
+}
+
+func upAddUsersSearchIndex(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+			ALTER TABLE users ADD INDEX users_search (first_name, last_name)
+		`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func downAddUsersSearchIndex(tx *sql.Tx) error {
+	_, err := tx.Exec(`
+			ALTER TABLE users DROP INDEX users_search;
+		`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
