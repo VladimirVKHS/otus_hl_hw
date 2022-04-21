60b412e88d44cf5451cd7822037dbb054a7f3ed0
Vladimir <kovalev.home@mail.ru>
2022-04-21 14:27:59 +0300

    [~] app ->docker

--- docker/mysql/scripts/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'otus'@'%' IDENTIFIED BY 'otus';
-CREATE DATABASE otus;
-GRANT ALL PRIVILEGES ON otus.* TO 'otus'@'%';
-GRANT replication slave ON *.* TO 'otus'@'%';
-GRANT REPLICATION CLIENT ON *.* TO 'otus'@'%';
-GRANT SELECT ON *.* TO 'otus'@'%';
-FLUSH PRIVILEGES
